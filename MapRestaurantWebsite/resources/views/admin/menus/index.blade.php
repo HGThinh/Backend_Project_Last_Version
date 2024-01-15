27d066400c6207dd272dcd609aacf8426fdd09ce
HGThinh <132908713+HGThinh@users.noreply.github.com>
2024-01-15 09:53:59 +0100

    Add button create new

--- MapRestaurantWebsite/resources/views/admin/menus/index.blade.php
+++ MapRestaurantWebsite/resources/views/admin/menus/index.blade.php
@@ -12,6 +12,9 @@
                     {{ __('Menus') }}
                 </div>
             </div>
+            <div>
+                <a href="{{ route('admin.menus.create') }}"> New Menu </a>
+            </div>
             <div class="relative overflow-x-auto">
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
--- MapRestaurantWebsite/resources/views/admin/tables/index.blade.php
+++ MapRestaurantWebsite/resources/views/admin/tables/index.blade.php
@@ -12,6 +12,9 @@
                     {{ __('Tables') }}
                 </div>
             </div>
+            <div>
+                <a href="{{ route('admin.tables.create') }}"> New Table </a>
+            </div>
             <div class="relative overflow-x-auto">
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
