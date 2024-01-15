2aea45657f67a58d332f52573091367ca2ef4908
HGThinh <132908713+HGThinh@users.noreply.github.com>
2024-01-15 14:21:27 +0100

    Update create.blade.php

--- MapRestaurantWebsite/resources/views/admin/tables/create.blade.php
+++ MapRestaurantWebsite/resources/views/admin/tables/create.blade.php
@@ -25,6 +25,10 @@
                             Create New Table
                         </label>
                         <div class="mt-1">
+                            <label for="name" class="block text-sm font-medium text-gray-700">
+                                Name
+                            </label>
+
                             <input type="text" id="name" name="name"
                                 class="block w-full  appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                         </div>
@@ -56,34 +60,6 @@
                 </form>
             </div>
 
-            <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
-                <form method="POST" action="{{ route('admin.tables.store') }}" enctype="multipart/form-data">
-                    @csrf
-                    <div class="sm:col-span-6">
-                        <label for="title" class="block text-sm font-medium text-gray-700"> Post Title </label>
-                        <div class="mt-1">
-                            <input type="text" id="title" name="title"
-                                class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
-                        </div>
-                    </div>
-                    <div class="sm:col-span-6">
-                        <label for="title" class="block text-sm font-medium text-gray-700"> Post Image </label>
-                        <div class="mt-1">
-                            <input type="file" id="image" name="image"
-                                class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
-                        </div>
-                    </div>
-
-                    <div class="sm:col-span-6 pt-5">
-                        <label for="body" class="block text-sm font-medium text-gray-700">Body</label>
-                        <div class="mt-1">
-                            <textarea id="body" rows="3"
-                                class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
-                        </div>
-                    </div>
-                    <button type="submit">Store</button>
-                </form>
-            </div>
 
 
         </div>
