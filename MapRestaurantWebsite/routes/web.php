7c8216b59162d3dfc712f85dd8a114a448c0aef9
HGThinh <132908713+HGThinh@users.noreply.github.com>
2024-01-15 17:07:46 +0100

    Update web.php

--- MapRestaurantWebsite/routes/web.php
+++ MapRestaurantWebsite/routes/web.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Route for guest
+Route::get('/test', function () {
+    return view('test')->name('test');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -36,5 +41,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+
 
 require __DIR__ . '/auth.php';
