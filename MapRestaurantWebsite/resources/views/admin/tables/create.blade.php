d2e5e7f5b09c4d29fa29c1ab92e989b26e8c27a6
HGThinh <132908713+HGThinh@users.noreply.github.com>
2024-01-15 14:29:05 +0100

    Update create.blade.php

--- MapRestaurantWebsite/resources/views/admin/tables/create.blade.php
+++ MapRestaurantWebsite/resources/views/admin/tables/create.blade.php
@@ -24,6 +24,7 @@
                         <label for="title" class="block text-sm font-medium text-gray-700">
                             Create New Table
                         </label>
+
                         <div class="mt-1">
                             <label for="name" class="block text-sm font-medium text-gray-700">
                                 Name
@@ -32,36 +33,38 @@
                             <input type="text" id="name" name="name"
                                 class="block w-full  appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                         </div>
+
                         <div class="col-span-6">
-                            <label for="guest_number" class="block text-sm font-medium text-gray-700">Guest
-                                Number</label>
+                            <label for="guest_number" class="block text-sm font-medium text-gray-700">
+                                GuestNumber
+                            </label>
                             <input type="number" id="guest_number" name="guest_number" />
                         </div>
-                        <div class="col-span-6">
-                            <label for="status" class="block text-sm font-medium text-gray-700">Status</label>
-                            <select id="status" name="status" autocomplete="country-name"
-                                class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                                <option value=""></option>
-                                <option></option>
-                                <option></option>
-                            </select>
-                        </div>
+
                         <div class="col-span-6">
                             <label for="location" class="block text-sm font-medium text-gray-700">Location</label>
-                            <select id="location" name="location" autocomplete="country-name"
+                            <select id="location" name="location"
                                 class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                                <option value=""></option>
-                                <option></option>
-                                <option></option>
+                                @foreach (App\Enums\TableLocation::cases() as $location)
+                                    <option value="{{ $location->value }}">{{ $location->name }}</option>
+                                @endforeach
+
+                            </select>
+                        </div>
+
+                        <div class="col-span-6">
+                            <label for="status" class="block text-sm font-medium text-gray-700">Status</label>
+                            <select id="status" name="status"
+                                class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                @foreach (App\Enums\TableStatus::cases() as $status)
+                                    <option value="{{ $status->value }}">{{ $status->name }}</option>
+                                @endforeach
                             </select>
                         </div>
                     </div>
                     <button type="submit">Store</button>
                 </form>
             </div>
-
-
-
         </div>
     </div>
 </x-admin-layout>
