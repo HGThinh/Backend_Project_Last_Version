0e9259501c2f61f7981516a2364c9bf10ae596be
HGThinh <132908713+HGThinh@users.noreply.github.com>
2024-01-15 16:04:54 +0100

    create controller

---
+++ MapRestaurantWebsite/app/Http/Controllers/Admin/ContactController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ContactStoreRequest;
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $tables = Contact::all();
+        return view('admin.contacts.index', compact('contacts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+        return view('admin.contacts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ContactStoreRequest $request)
+    {
+        //
+        Contact::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'reason' => $request->reason,
+        ]);
+
+        return to_route('admin.contacts.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Contact $contact)
+    {
+        //
+        return view('admin.contacts.edit', compact('contacts'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ContactStoreRequest $request, Contact $contact)
+    {
+        //
+        $contact->update($request->validated());
+        return to_route('admin.contacts.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Contact $contact)
+    {
+        //
+        $contact->delete();
+        return to_route('admin.contacts.index');
+    }
+}
