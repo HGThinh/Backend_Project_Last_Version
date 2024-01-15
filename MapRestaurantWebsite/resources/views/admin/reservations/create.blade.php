6ffc3ea73c071e96c6307ef4377c105b3d77e28b
HGThinh <132908713+HGThinh@users.noreply.github.com>
2024-01-15 10:05:29 +0100

    create create file

---
+++ MapRestaurantWebsite/resources/views/admin/reservations/create.blade.php
@@ -0,0 +1,59 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __('Reservations') }}
+                </div>
+            </div>
+
+            <div>
+                <a href="{{ route('admin.reservations.index') }}"> Reservation Index</a>
+            </div>
+
+            <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
+                <form enctype="multipart/form-data">
+                    <div class="sm:col-span-6">
+                        <label for="title" class="block text-sm font-medium text-gray-700"> Post Title </label>
+                        <div class="mt-1">
+                            <input type="text" id="title" wire:model.lazy="title" name="title"
+                                class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                        </div>
+                    </div>
+                    <div class="sm:col-span-6">
+                        <label for="title" class="block text-sm font-medium text-gray-700"> Post Image </label>
+                        <div class="mt-1">
+                            <input type="file" id="image" wire:model="newImage" name="image"
+                                class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                        </div>
+                    </div>
+                    <div class="col-span-6">
+                        <label for="country" class="block text-sm font-medium text-gray-700">Country</label>
+                        <select id="country" name="country" autocomplete="country-name"
+                            class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                            <option>United States</option>
+                            <option>Canada</option>
+                            <option>Mexico</option>
+                        </select>
+                    </div>
+                    <div class="sm:col-span-6 pt-5">
+                        <label for="body" class="block text-sm font-medium text-gray-700">Body</label>
+                        <div class="mt-1">
+                            <textarea id="body" rows="3" wire:model.lazy="body"
+                                class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
+                        </div>
+                    </div>
+                    <button type="submit">Store</button>
+                </form>
+            </div>
+
+
+        </div>
+    </div>
+</x-admin-layout>
