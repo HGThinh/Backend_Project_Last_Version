fb0e375ecbc1b325e49c93aa5b3154a3bbc3f91b
HGThinh <132908713+HGThinh@users.noreply.github.com>
2024-01-15 14:28:59 +0100

    Update Table.php
    add protected for enum

--- MapRestaurantWebsite/app/Models/Table.php
+++ MapRestaurantWebsite/app/Models/Table.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Enums\TableLocation;
+use App\Enums\TableStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +11,9 @@ class Table extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'guest_number', 'status', 'location'];
+
+    protected $casts = [
+        'status' => TableStatus::class,
+        'location' => TableLocation::class,
+    ];
 }
