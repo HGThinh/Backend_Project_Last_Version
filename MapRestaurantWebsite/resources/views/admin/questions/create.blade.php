961091f1b556ce9e89a18c6ad228a08f86bf2dfd
HGThinh <132908713+HGThinh@users.noreply.github.com>
2024-01-15 15:26:24 +0100

    Clone the folder

---
+++ MapRestaurantWebsite/resources/views/admin/questions/create.blade.php
@@ -0,0 +1,70 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __('Tables') }}
+                </div>
+            </div>
+
+            <div>
+                <a href="{{ route('admin.tables.index') }}"> Table Index</a>
+            </div>
+
+            <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
+                <form method="POST" action="{{ route('admin.tables.store') }}">
+                    @csrf
+                    <div class="sm:col-span-6">
+                        <label for="title" class="block text-sm font-medium text-gray-700">
+                            Create New Table
+                        </label>
+
+                        <div class="mt-1">
+                            <label for="name" class="block text-sm font-medium text-gray-700">
+                                Name
+                            </label>
+
+                            <input type="text" id="name" name="name"
+                                class="block w-full  appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                        </div>
+
+                        <div class="col-span-6">
+                            <label for="guest_number" class="block text-sm font-medium text-gray-700">
+                                GuestNumber
+                            </label>
+                            <input type="number" id="guest_number" name="guest_number" />
+                        </div>
+
+                        <div class="col-span-6">
+                            <label for="location" class="block text-sm font-medium text-gray-700">Location</label>
+                            <select id="location" name="location"
+                                class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                @foreach (App\Enums\TableLocation::cases() as $location)
+                                    <option value="{{ $location->value }}">{{ $location->name }}</option>
+                                @endforeach
+
+                            </select>
+                        </div>
+
+                        <div class="col-span-6">
+                            <label for="status" class="block text-sm font-medium text-gray-700">Status</label>
+                            <select id="status" name="status"
+                                class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                @foreach (App\Enums\TableStatus::cases() as $status)
+                                    <option value="{{ $status->value }}">{{ $status->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <button type="submit">Store</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</x-admin-layout>
---
+++ MapRestaurantWebsite/resources/views/admin/questions/index.blade.php
@@ -0,0 +1,68 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="">
+        <div class="">
+            <div class="">
+                <div class="">
+                    {{ __('Tables') }}
+                </div>
+            </div>
+            <div>
+                <a href="{{ route('admin.tables.create') }}"> New Table </a>
+            </div>
+            <div class="relative overflow-x-auto">
+                <table class="">
+                    <thead class="">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Guest
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Status
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Location
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($tables as $table)
+                            <tr class="">
+                                <th scope="row" class="">
+                                    {{ $table->name }}
+                                </th>
+                                <td class="">
+                                    {{ $table->guest_number }}
+                                </td>
+                                <td class="">
+                                    {{ $table->location->name }}
+                                </td>
+                                <td class="">
+                                    {{ $table->status->name }}
+                                </td>
+                                <td class="">
+                                    <a href="{{ route('admin.tables.edit', $table->id) }}">Edit</a>
+                                    <form method="POST" action="{{ route('admin.tables.destroy', $table->id) }}"
+                                        onsubmit="return confirm ('Areyousure?');">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</x-admin-layout>
